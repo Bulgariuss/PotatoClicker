244d11081fbd893313dd72e8347baad594940d2f
Kuba Szczęsnowicz <kubalek00@gmail.com>
2022-08-22 16:23:57 +0200

    teest

--- src/components/Music.js
+++ src/components/Music.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import bgmusic from '../bgmusic.mp3';
 const useAudio = url => {
   const [audio] = useState(new Audio(url));
-  const [playing, setPlaying] = useState(false);
+  const [playing, setPlaying] = useState(true);
 
   const toggle = () => setPlaying(!playing);
  
